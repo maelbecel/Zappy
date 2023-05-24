7a4ffa8c349f9f29eb22ce5745ca92a43a733036
jovanHillion <jovan.hillion@epitech.eu>
2023-05-25 00:03:12 +0200

    feat (.github/workflows/ai-checker.yml && src/clientAi/ && test/clientAi): git action to implement coding style checker && code refactor && run uniTest [#1]

---
+++ src/clientAi/main.py
@@ -0,0 +1,22 @@
+##
+## EPITECH PROJECT, 2023
+## endYearPorject
+## File description:
+## main
+##
+
+def main    (          )  :
+
+
+
+
+
+                    print(                        'Hello World'        )
+
+if                 __name__ == "__main__"                           :
+
+
+
+
+
+    main()

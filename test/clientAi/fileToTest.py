2bd1d8a611ff9c0a49ad80f84a01f56958345b1a
jovanHillion <jovan.hillion@epitech.eu>
2023-05-25 00:21:23 +0200

    fix (.github/workflows/ai-checker.yml && test/clientAi): rename default commit and fix functionnal tests [#9]

--- test/clientAi/fileToTest.py
+++ test/clientAi/fileToTest.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 class TryTesting(TestCase):
     def test_always_true_1(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
     def test_always_true_2(self):
         self.assertFalse(False)

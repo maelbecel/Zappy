ca3def07ed6311edc2459d8395b428015ec0ac34
GitHub Action <action@github.com>
2023-05-24 22:46:04 +0000

    fix (./src/clientAi): coding style by gitAction

--- src/clientAi/main.py
+++ src/clientAi/main.py
@@ -5,18 +5,10 @@
 ## main
 ##
 
-def main    (          )  :
+
+def main():
+    print("Hello World")
 
 
-
-
-
-                    print(                                           'Hello World'        )
-
-if                 __name__ == "__main__"                           :
-
-
-
-
-
+if __name__ == "__main__":
     main()

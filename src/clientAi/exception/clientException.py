192d9b525b0b379e32b24579bc41cceab909a8b8
jovanHillion <jovan.hillion@epitech.eu>
2023-05-30 14:52:35 +0200

    feat (./src/clientAI/): functionnal client able to interact with the server without ai logic

---
+++ src/clientAi/exception/clientException.py
@@ -0,0 +1,15 @@
+##
+## EPITECH PROJECT, 2023
+## Zappy
+## File description:
+## clientException
+##
+
+
+class clientException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
---
+++ src/clientAi/server/clientServer.py
@@ -0,0 +1,56 @@
+##
+## EPITECH PROJECT, 2023
+## Zappy
+## File description:
+## clientServer
+##
+
+import socket
+import exception.clientException as cEx
+
+
+class clientServer:
+    def __init__(self, port, host):
+        print("Init clientServer")
+        self.port = port  # port
+        self.host = host  # ip
+        self.socket = None
+
+    def connect(self):
+        # AF_INET = IPv4 / SOCK_STREAM = TCP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if self.socket == None:
+            cEx.clientException("Error: socket is null")
+        if self.socket.connect((self.host, int(self.port))) == -1:
+            cEx.clientException("Error: socket is not connect")
+        print("Connect to server")
+
+    def disconnect(self):
+        self.socket.close()
+        if self.socket != None:
+            cEx.clientException("Error: socket is not close")
+        print("Disconnect to server")
+
+    def send(self, message):
+        if self.socket == None:
+            cEx.clientException("Error: socket is null")
+        print("Send message to server: " + message + "\n")
+        self.socket.sendall(message.encode("ascii"))
+
+    def receive(self):
+        if self.socket == None:
+            cEx.clientException("Error: socket is null")
+        response = self.socket.recv(1024).decode()
+        if response == None:
+            print("Error: response is null")
+        print("Receive message from server: " + response + "\n")
+        return response
+
+    def getSocket(self):
+        return self.socket
+
+    def getPort(self):
+        return self.port
+
+    def getHost(self):
+        return self.host

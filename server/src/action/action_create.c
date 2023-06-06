d9cc93f48ac6874671d0b0b8f0be1d9c558607e0
maelbecel <mael1.becel@epitech.eu>
2023-06-06 16:31:01 +0200

    docs (src/action/): improve documentation

--- server/src/action/action_create.c
+++ server/src/action/action_create.c
@@ -9,6 +9,27 @@
 #include <string.h>
 #include "server.h"
 
+/**
+ * This function creates an action with a given name, server, client,
+ * and duration.
+ * 
+ * @param name A string representing the name of the action.
+ * @param server The server parameter is a pointer to a server_t struct,
+ * which likely contains information about the server such as its IP address,
+ * port number, and other relevant details. This parameter is used to
+ * associate the action with a specific server.
+ * @param client The client parameter is a pointer to a client_t struct,
+ * which likely contains information about a client connected to the server.
+ * This parameter is used to store a reference to the client in the action_t
+ * struct created by the action_create function.
+ * @param duration The duration parameter is of type ulong (unsigned long)
+ * and represents the duration
+ * of the action in some unit of time (e.g. seconds, milliseconds).
+ * 
+ * @return The function `action_create` returns a pointer to an `action_t`
+ * struct. If the allocation of
+ * memory for the struct or its `data` field fails, it returns `NULL`.
+ */
 action_t *action_create(char *name, server_t *server, client_t *client,
 ulong duration)
 {
--- server/src/action/action_update.c
+++ server/src/action/action_update.c
@@ -13,6 +13,20 @@
 #include "command.h"
 #include "olog.h"
 
+/**
+ * This function assigns the next action to a client from their waiting orders
+ * list.
+ * 
+ * @param client A pointer to a client_t struct, which likely contains
+ * information about a client connected to a server.
+ * @param server A pointer to a server_t struct, which likely contains
+ * information about the server and its current state.
+ * 
+ * @return If `client->current_action` is not NULL and
+ * `client->waiting_orders->size` is 0, then nothing is being returned and
+ * the function exits early. If `node` is not found or is NULL, then
+ * nothing is being returned and the function exits early.
+ */
 static void assign_next_action(client_t *client, server_t *server)
 {
     char *buffer = NULL;
@@ -31,6 +45,23 @@ static void assign_next_action(client_t *client, server_t *server)
     execute_waiting_order(client, server);
 }
 
+/**
+ * The function updates the food level of an AI client and triggers its death
+ * if it runs out of food.
+ * 
+ * @param client A pointer to a client structure, which likely contains
+ * information about a player or AI in the game.
+ * @param tickDiff tickDiff is the time difference between the current tick
+ * and the previous tick in the game. It is used to update the time before
+ * the AI dies and to check if the AI has enough food to survive.
+ * @param server The server parameter is a pointer to the server structure,
+ * which contains information about the game server that the client is
+ * connected to. This structure may include things like the current game
+ * state, player information, and other server-specific data.
+ * 
+ * @return an integer value. If the function is successful, it returns 0.
+ * If there is an error, it returns -1.
+ */
 static int update_food(client_t *client, uint tickDiff, server_t *server)
 {
     ai_t *ai = client->data;
@@ -50,6 +81,22 @@ static int update_food(client_t *client, uint tickDiff, server_t *server)
     return 0;
 }
 
+/**
+ * This function updates a client's actions and assigns a new action
+ * if the current one is completed or there are no current actions.
+ * 
+ * @param client A pointer to a client_t struct, which represents a client in
+ * the server.
+ * @param tickDiff tickDiff is a variable representing the time difference
+ * between the current tick and the previous tick in the game. It is used to
+ * update the duration of the current action of a client and to determine when
+ * to assign the next action to the client.
+ * @param server A pointer to a server_t struct, which likely contains
+ * information about the game server that the client is connected to.
+ * 
+ * @return If `update_food(client, tickDiff, server)` returns -1, the function
+ * will return without performing any further actions.
+ */
 static void update_client(client_t *client, uint tickDiff, server_t *server)
 {
     if (update_food(client, tickDiff, server) == -1)
@@ -68,6 +115,16 @@ static void update_client(client_t *client, uint tickDiff, server_t *server)
     }
 }
 
+/**
+ * This function updates all AI clients in the server with the time
+ * difference since the last update.
+ * 
+ * @param server a pointer to the server structure which contains information
+ * about the game server.
+ * 
+ * @return If `tickDiff` is equal to 0, the function returns without
+ * performing any further actions.
+ */
 void action_update(server_t *server)
 {
     uint tickDiff = 0;

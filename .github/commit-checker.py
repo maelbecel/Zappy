c787c920049b88503ddbcd6af72bdf8fd126bfb3
maelbecel <mael1.becel@epitech.eu>
2023-06-06 12:00:19 +0200

    fix (actions/): fix the commit checker [#1]

--- .github/commit-checker.py
+++ .github/commit-checker.py
@@ -2,7 +2,7 @@ import re
 import sys
 
 def check_commit_format(commit):
-    pattern = r'\w+\s\([\w\s,\.\/]+\)\:\s.+\s\[\#\d+\]$'
+    pattern = r'^\w+\s\([\w\s,\.\/]+\)\:\s.+$'
     if re.match(pattern, commit):
         print(commit + " [OK]")
         return 0

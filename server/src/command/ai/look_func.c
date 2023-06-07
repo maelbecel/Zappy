6f52f6f1d0d7570c7b060d9e8808de1f4f008221
Lucas <lucas1.dupont@epitech.eu>
2023-06-07 17:57:22 +0200

    fix(look_func): Remove whitespace

--- server/src/command/ai/look_func.c
+++ server/src/command/ai/look_func.c
@@ -21,10 +21,10 @@ void look_north(client_t *client, server_t *server, ai_t *ai)
             y = (y >= server->map->height) ? y - server->map->height : y;
             tile = map_get_tile(server->map, x, y);
             print_tile_content(tile, client);
-            (j != i) ? dprintf(client->socket->fd, ", ") : 0;
+            (j != i) ? dprintf(client->socket->fd, ",") : 0;
         }
         if (i != (int)ai->level)
-            dprintf(client->socket->fd, ", ");
+            dprintf(client->socket->fd, ",");
     }
 }
 
@@ -43,10 +43,10 @@ void look_south(client_t *client, server_t *server, ai_t *ai)
             y = (y >= server->map->height) ? y - server->map->height : y;
             tile = map_get_tile(server->map, x, y);
             print_tile_content(tile, client);
-            (j != -i) ? dprintf(client->socket->fd, ", ") : 0;
+            (j != -i) ? dprintf(client->socket->fd, ",") : 0;
         }
         if (i != (int)ai->level)
-            dprintf(client->socket->fd, ", ");
+            dprintf(client->socket->fd, ",");
     }
 }
 
@@ -64,10 +64,10 @@ void look_west(client_t *client, server_t *server, ai_t *ai)
             y = (y >= server->map->height) ? y - server->map->height : y;
             tile = map_get_tile(server->map, x, y);
             print_tile_content(tile, client);
-            (j != -i) ? dprintf(client->socket->fd, ", ") : 0;
+            (j != -i) ? dprintf(client->socket->fd, ",") : 0;
         }
         if (i != (int)ai->level)
-            dprintf(client->socket->fd, ", ");
+            dprintf(client->socket->fd, ",");
     }
 }
 
@@ -86,9 +86,9 @@ void look_east(client_t *client, server_t *server, ai_t *ai)
             y = (y >= server->map->height) ? y - server->map->height : y;
             tile = map_get_tile(server->map, x, y);
             print_tile_content(tile, client);
-            (j != i) ? dprintf(client->socket->fd, ", ") : 0;
+            (j != i) ? dprintf(client->socket->fd, ",") : 0;
         }
         if (i != (int)ai->level)
-            dprintf(client->socket->fd, ", ");
+            dprintf(client->socket->fd, ",");
     }
 }

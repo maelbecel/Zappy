9b00b033fbf2e5399b12db49369d5051685c8dc6
maelbecel <mael1.becel@epitech.eu>
2023-06-08 11:18:28 +0200

    docs (utils/): improve documentation

--- server/src/utils/read_socket.c
+++ server/src/utils/read_socket.c
@@ -11,6 +11,18 @@
 #include "olog.h"
 #include "client.h"
 
+/**
+ * The function reads data from a client's socket until a newline
+ * character is encountered.
+ *
+ * @param client a pointer to a client_t struct, which contains
+ * information about the client, including their socket file descriptor
+ * and a buffer to store incoming data.
+ *
+ * @return The function does not explicitly return anything, but it may
+ * return prematurely if there is an error reading from the client or if the
+ * client disconnects.
+ */
 static void do_read(client_t *client)
 {
     int read_size = 0;
@@ -31,6 +43,21 @@ static void do_read(client_t *client)
     }
 }
 
+/**
+ * The function reads data from a socket and stores it in a buffer, removing
+ * the newline character at the end if present.
+ *
+ * @param client A pointer to a client_t struct, which likely contains
+ * information about a client's connection to a server.
+ *
+ * @return If the allocation of memory for the client buffer fails, the
+ * function returns without doing anything. Otherwise, the function reads
+ * data from the client socket and stores it in the buffer. If the buffer is
+ * not empty and contains a newline character at the end, the newline
+ * character is replaced with a null terminator. The function does not
+ * explicitly return a value, but it modifies the client buffer and may
+ * return early if
+ */
 void read_socket(client_t *client)
 {
     if (client->buffer) {

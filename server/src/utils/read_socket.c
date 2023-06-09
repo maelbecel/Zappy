cbffb47da567389d22af6a5a089d942297eb6098
Lucas <lucas1.dupont@epitech.eu>
2023-06-09 17:34:26 +0200

    fix(read_socket.c): Add read buffer for message size over 4096

--- server/src/utils/read_socket.c
+++ server/src/utils/read_socket.c
@@ -8,68 +8,62 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "olog.h"
 #include "client.h"
+#include "olog.h"
 
-/**
- * The function reads data from a client's socket until a newline
- * character is encountered.
- *
- * @param client a pointer to a client_t struct, which contains
- * information about the client, including their socket file descriptor
- * and a buffer to store incoming data.
- *
- * @return The function does not explicitly return anything, but it may
- * return prematurely if there is an error reading from the client or if the
- * client disconnects.
- */
-static void do_read(client_t *client)
+static void do_read(client_t *client, olist_t *buffer)
 {
     int read_size = 0;
+    char *tmp = calloc(MAX_READ_SIZE + 1, sizeof(char));
+    bool found = false;
 
-    while (strlen(client->buffer) < 1
-    || client->buffer[strlen(client->buffer) - 1] != '\n') {
-        read_size = read(client->socket->fd, client->buffer +
-        strlen(client->buffer), MAX_READ_SIZE);
-        if (read_size == 0) {
-            free(client->buffer);
-            client->buffer = NULL;
-            return;
-        }
-        if (read_size == -1) {
-            OLOG_ERROR("Failed to read from client #%ld", client->id);
-            return;
-        }
+    if (!tmp)
+        return;
+    while (!found) {
+        read_size = read(client->socket->fd, tmp, MAX_READ_SIZE);
+        if (read_size <= 0)
+            break;
+        if (read_size < MAX_READ_SIZE)
+            found = true;
+        tmp[read_size] = '\0';
+        olist_add_node(buffer, strdup(tmp));
+        memset(tmp, 0, MAX_READ_SIZE);
     }
+    free(tmp);
 }
 
-/**
- * The function reads data from a socket and stores it in a buffer, removing
- * the newline character at the end if present.
- *
- * @param client A pointer to a client_t struct, which likely contains
- * information about a client's connection to a server.
- *
- * @return If the allocation of memory for the client buffer fails, the
- * function returns without doing anything. Otherwise, the function reads
- * data from the client socket and stores it in the buffer. If the buffer is
- * not empty and contains a newline character at the end, the newline
- * character is replaced with a null terminator. The function does not
- * explicitly return a value, but it modifies the client buffer and may
- * return early if
- */
+static char *empty_buffer(olist_t *buffer)
+{
+    char *msg = NULL;
+    char *tmp = NULL;
+
+    if (!buffer || buffer->size == 0)
+        return NULL;
+    OLIST_FOREACH(buffer, node) {
+        tmp = (char *)node->data;
+        if (!tmp)
+            continue;
+        if (!msg)
+            asprintf(&msg, "%s", tmp);
+        else
+            asprintf(&msg, "%s%s", msg, tmp);
+    }
+    olist_clear_wdfree(buffer);
+    if (!msg)
+        return NULL;
+    return msg;
+}
+
 void read_socket(client_t *client)
 {
+    olist_t *buffer = olist_create();
+
     if (client->buffer) {
         free(client->buffer);
         client->buffer = NULL;
     }
-    client->buffer = calloc(MAX_READ_SIZE, sizeof(char));
-    if (!client->buffer) {
-        OLOG_FATALA("Failed to allocate memory for client buffer");
-        return;
-    }
-    do_read(client);
+    do_read(client, buffer);
+    client->buffer = empty_buffer(buffer);
     if (!client->buffer)
         return;
     if (client->buffer[strlen(client->buffer) - 1] == '\n')

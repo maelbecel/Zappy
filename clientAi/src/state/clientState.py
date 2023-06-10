8f8dc40cdc97e5049e42b2c09d7b7b555d60cb87
jovanHillion <jovan.hillion@epitech.eu>
2023-06-10 18:39:37 +0200

    feat (./clientAi/src/state/): add state class to define some stat of the AI

---
+++ clientAi/src/state/clientState.py
@@ -0,0 +1,14 @@
+##
+## EPITECH PROJECT, 2023
+## Zappy
+## File description:
+## clientState
+##
+
+from enum import Enum
+
+
+class enumState(Enum):
+    NEED_FOOD = 0
+    LF_RESSOURCES = 1
+    FULL_RESSOUCRES = 2

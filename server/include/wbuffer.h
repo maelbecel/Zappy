bed9135f6310d053220281d11b496c7223d4b8ac
Lucas <lucas1.dupont@epitech.eu>
2023-06-13 17:55:29 +0200

    fix(Makefile): Add _GNU_SOURCE directly in CFLAGS to avoid warnings

--- server/include/wbuffer.h
+++ server/include/wbuffer.h
@@ -8,10 +8,10 @@
 #ifndef WBUFFER_H_
     #define WBUFFER_H_
 
-    #include "olog.h"
     #include <stddef.h>
     #include <stdarg.h>
     #include "client.h"
+    #include "olog.h"
 
 void wbuffer_add_message(client_t *client, char *fmt, ...);
 void wbuffer_add_msg(client_t *client, char *msg);
